92ec43dba34322407a773d66665d3370e842662c
uoclnotario <72309408+uoclnotario@users.noreply.github.com>
2021-03-21 19:39:46 +0100

    arreglos para que funcionen imagens por recurso

--- 4puzzleGame/app/src/main/java/com/puzzle/Game/lyLogicalBusiness/Picture.kt
+++ 4puzzleGame/app/src/main/java/com/puzzle/Game/lyLogicalBusiness/Picture.kt
@@ -5,6 +5,8 @@ import java.io.Serializable
 
 class Picture : Serializable {
     lateinit var fileDir: String
+     var imgResource : Int = 0
+
     lateinit var hasPicture: String
     lateinit var rute: String
     var realWidth: Int = 0
