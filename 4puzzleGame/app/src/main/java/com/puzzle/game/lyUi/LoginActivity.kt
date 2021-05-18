e4922103f9c295f65e76abeec67d635d7acb8435
uoclnotario <72309408+uoclnotario@users.noreply.github.com>
2021-05-19 00:02:27 +0200

    AuthGoogle

--- 4puzzleGame/app/src/main/java/com/puzzle/game/lyUi/LoginActivity.kt
+++ 4puzzleGame/app/src/main/java/com/puzzle/game/lyUi/LoginActivity.kt
@@ -1,15 +1,19 @@
 package com.puzzle.game.lyUi
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
 import com.puzzle.game.R
 import com.puzzle.game.lyLogicalBusiness.Player
 import kotlinx.android.synthetic.main.activity_onlinelogin.*
+
 
 class LoginActivity : AppCompatActivity() {
     val RC_SIGN_IN = 121
@@ -24,35 +28,53 @@ class LoginActivity : AppCompatActivity() {
         player = intent.getSerializableExtra("player") as Player
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken("93082884989-ukfh05r1oepfbqr0h5dnno05ebeokpv0.apps.googleusercontent.com")
             .requestEmail()
             .build()
 
         var googleSignInClient = GoogleSignIn.getClient(this, gso)
 
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        println("cuenta: "+ account.toString())
+
+
+
         google_button.setOnClickListener{
-            val signInIntent = googleSignInClient.signInIntent
-            startActivityForResult(signInIntent, RC_SIGN_IN)
+            // Choose authentication providers
+            val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+
+            // Create and launch sign-in intent
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN)
+
         }
 
     }
 
 
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                println("firebaseAuthWithGoogle:" + account.id.toString())
-              //  firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                println("Google sign in failed"+e.message)
+            val response = IdpResponse.fromResultIntent(data)
+            println(resultCode.toString())
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                println("Autenticación correcta" + user.toString())
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+                if (response != null) {
+                    println("Fallo al conectar"+ response.getError()?.getErrorCode().toString())
+                }
+
             }
         }
     }
