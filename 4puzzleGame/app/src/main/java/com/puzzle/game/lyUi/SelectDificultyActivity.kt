5b8fb073544d4cc1e7ff6818d927f484bca87c5a
Ricardo <72918409+rmoyanom@users.noreply.github.com>
2021-05-25 09:21:05 +0200

    sdk

--- 4puzzleGame/app/src/main/java/com/puzzle/game/lyUi/SelectDificultyActivity.kt
+++ 4puzzleGame/app/src/main/java/com/puzzle/game/lyUi/SelectDificultyActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import butterknife.BindView
 import com.puzzle.game.lyLogicalBusiness.Picture
 import com.puzzle.game.R
 import com.puzzle.game.lyLogicalBusiness.Player
@@ -16,6 +17,11 @@ class SelectDificultyActivity : AppCompatActivity() {
      lateinit var _player : Player
      var _pictur = Picture(null)
      var _modGame = 1
+
+    @BindView(R.id.btnEasy) lateinit var btn_click_easy : Button
+    @BindView(R.id.btnMedium) lateinit var btn_click_medium_ : Button
+    @BindView(R.id.btnHard) lateinit var btn_click_hard : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selectdificulty)
@@ -23,9 +29,9 @@ class SelectDificultyActivity : AppCompatActivity() {
         _pictur = intent.getSerializableExtra("pictur") as Picture
         _modGame = intent.getIntExtra("tipoJuego",1)
 
-        val btn_click_easy = findViewById(R.id.btnEasy) as Button
-        val btn_click_medium_ = findViewById(R.id.btnMedium) as Button
-        val btn_click_hard = findViewById(R.id.btnHard) as Button
+        //val btn_click_easy = findViewById(R.id.btnEasy) as Button
+        //val btn_click_medium_ = findViewById(R.id.btnMedium) as Button
+        //val btn_click_hard = findViewById(R.id.btnHard) as Button
 
         btn_click_easy.setOnClickListener{
             launchGame(1)
